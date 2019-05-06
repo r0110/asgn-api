23cc7394125244b84e29e490e739310f1bf40ef4
Josh Forbord <48929764+r0110@users.noreply.github.com>
2019-05-06 13:42:50 -0500

    Initial commit, set up entry point, model schema, & gitignore files

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express(); 
+const PORT = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+
+//Routes
+
+//Database
+var mongoose = require('mongoose')
