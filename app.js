228750c6527391b2810a5ec675af0fbc72d97cbf
Josh Forbord <48929764+r0110@users.noreply.github.com>
2019-05-08 20:36:48 -0500

    add app.use statements to connect model and controller to app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var app = express();
 const PORT = process.env.PORT || 3500;
 var bodyParser = require('body-parser');
 
-//Routes
+//Router
+var asgnRoute = require('asgn-router');
 
 //Database
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/assignments', {useNewUrlParser: true});
+
+app.use(bodyParser.json());
+app.use(asgnRoute);
 
 app.listen(PORT, () => {
     console.log('API running on ' + PORT);
