470bb3d8d5168fcb679baa1ab5e146739a24f7cf
Josh Forbord <48929764+r0110@users.noreply.github.com>
2019-05-08 19:47:06 -0500

    set up basic express server with connection to local MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express(); 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3500;
 var bodyParser = require('body-parser');
 
 //Routes
 
 //Database
 var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/assignments', {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log('API running on ' + PORT);
+});
